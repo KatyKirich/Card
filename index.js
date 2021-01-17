6004af28a3d3da63cd69a918de8be9276a38a7d8
Kate <katy8kirichenko@gmail.com>
2021-01-17 20:12:08 +0300

    function getSearch

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const btnSubmit = document.getElementById("btnSubmit");
 const data = [];
-const desk = document.querySelector('.desk')
+const desk = document.querySelector(".desk");
 
-function showCard(){
-    data.forEach ((item)=>{
-      desk.innerHTML += ` <div><p>Title:${item.title}</p>
+function showCard() {
+  data.forEach((item) => {
+    desk.innerHTML += ` <div><p>Title:${item.title}</p>
       <p>Description: ${item.description}</p>
-    <button>Delete</button>
-  <button>Edit</button></div> <br>
-  <br>`})
-
-    
+    <button id="button">Delete</button>
+  <button id="button">Edit</button></div> <br>
+  <br>`;
+  });
 }
+
+const button = document.getElementById("button");
+
+function getSearch() {}
+
+button.addEventListener("click", (event) => {
+  if (event.target.closest("#button")) {
+    if (event.target.textContent === "Delete") {
+    }
+    if (event.target.textContent === "Edit") {
+    }
+  }
+});
 
 btnSubmit.addEventListener("click", (event) => {
   event.preventDefault();
@@ -21,5 +33,5 @@ btnSubmit.addEventListener("click", (event) => {
   data.push({ title: title.value, description: dscr.value });
   console.log(data);
 
-  showCard()
+  showCard();
 });
