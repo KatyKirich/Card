c457f9de073fca246386c5da4ad1c6002707c44d
Kate <katy8kirichenko@gmail.com>
2021-01-18 22:39:42 +0300

    forEach

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const data = [];
 const desk = document.querySelector(".desk");
 
 function showCard() {
-  desk.innerHTML =""
-    data.forEach((item) => {
+  desk.innerHTML = "";
+  data.forEach((item) => {
     desk.innerHTML += ` <div class="card"><p class = "title">Title:${item.title}</p>
       <p  class = "description">Description: ${item.description}</p>
     <button id="delBtn">Delete</button>
@@ -14,18 +14,17 @@ function showCard() {
 }
 
 function getSearch(event) {
- const div = event.target.closest(".card");
+  const div = event.target.closest(".card");
 
-  const cardTitle = div.querySelector(".title");
-  const cardDscr = div.querySelector(".description");
- 
-  let textTitle = cardTitle.textContent;
-  let textDscr = cardDscr.textContent;
- console.log()
-  
+  const cardTitle = div.querySelector(".title").textContent;
+  const cardDscr = div.querySelector(".description").textContent;
+
+  data.forEach(function (elem, index) {
+    if (elem.title === cardTitle && elem.description === cardDscr) {
+      console.log(index);
+    }
+  });
 }
-
-
 
 btnSubmit.addEventListener("click", (event) => {
   event.preventDefault();
@@ -33,19 +32,16 @@ btnSubmit.addEventListener("click", (event) => {
   const dscr = document.getElementById("description");
 
   data.push({ title: title.value, description: dscr.value });
- 
+
   showCard();
- 
 });
 
 desk.addEventListener("click", (event) => {
   event.preventDefault();
   if (event.target.closest("#delBtn")) {
-    
-      getSearch(event)
-    }
-    if (event.target.closest("#editBtn")) {
-      getSearch(event)
-    }
+    getSearch(event);
   }
-);
+  if (event.target.closest("#editBtn")) {
+    getSearch(event);
+  }
+});
