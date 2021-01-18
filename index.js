84ecaac545f3b95c0abcea539daf53f41990b924
Kate <katy8kirichenko@gmail.com>
2021-01-18 23:18:24 +0300

    index

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const desk = document.querySelector(".desk");
 function showCard() {
   desk.innerHTML = "";
   data.forEach((item) => {
-    desk.innerHTML += ` <div class="card"><p class = "title">Title:${item.title}</p>
-      <p  class = "description">Description: ${item.description}</p>
+    desk.innerHTML += ` <div class="card"><p>Title:</p><p class = "title">${item.title}</p> <br /><p>Description:</p>
+      <p  class = "description">${item.description}</p>
     <button id="delBtn">Delete</button>
   <button id="editBtn">Edit</button></div> <br>
   <br>`;
