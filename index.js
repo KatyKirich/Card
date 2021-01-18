03688af56468ab7b8700db70c5842cd774b52c8c
Kate <katy8kirichenko@gmail.com>
2021-01-18 18:07:05 +0300

    value

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const data = [];
 const desk = document.querySelector(".desk");
 
 function showCard() {
-  data.forEach((item) => {
-    desk.innerHTML += ` <div class = "card"><p class = "title">Title:${item.title}</p>
+  desk.innerHTML =""
+    data.forEach((item) => {
+    desk.innerHTML += ` <div class="card"><p class = "title">Title:${item.title}</p>
       <p  class = "description">Description: ${item.description}</p>
     <button id="delBtn">Delete</button>
   <button id="editBtn">Edit</button></div> <br>
@@ -15,7 +16,12 @@ function showCard() {
 function getSearch(event) {
  const div = event.target.closest(".card");
 
- console.log(div)
+  const cardTitle = div.querySelector(".title");
+  const cardDscr = div.querySelector(".description");
+ 
+  let textTitle = cardTitle.textContent;
+  let textDscr = cardDscr.textContent;
+ console.log()
   
 }
 
@@ -29,16 +35,17 @@ btnSubmit.addEventListener("click", (event) => {
   data.push({ title: title.value, description: dscr.value });
  
   showCard();
+ 
 });
 
 desk.addEventListener("click", (event) => {
   event.preventDefault();
   if (event.target.closest("#delBtn")) {
     
-      getSearch()
+      getSearch(event)
     }
     if (event.target.closest("#editBtn")) {
-      getSearch()
+      getSearch(event)
     }
   }
 );
